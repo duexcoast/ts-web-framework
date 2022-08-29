d1b0d07cefba7804544d527548470ebf4d537145
conor.ney <conor.ux@gmail.com>
2022-08-29 01:12:47 -0400

    optional interface on UserProps

--- src/User.ts
+++ src/User.ts
@@ -6,7 +6,8 @@ export class User {
   constructor(private data: UserProps) {}
 
   get(propName: string): string | number {
-    return this.data[propName as keyof UserProps];
+    // @ts-ignore:
+    return this.data[propName! as keyof UserProps];
   }
 
   set(update: UserProps): void {
