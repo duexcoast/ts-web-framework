f43471f55b8edede98a7be1efe7363eac21d3fb4
conor.ney <conor.ux@gmail.com>
2022-09-03 23:49:10 -0400

    added fetch method

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,4 @@
 import { User } from './User';
+import axios from 'axios';
 
-const user = new User({ name: 'myname', age: 20 });
-
-user.on('change', () => {
-  console.log('change #1');
-});
-
-user.on('change', () => {
-  console.log('change #2');
-});
-user.on('save', () => {
-  console.log('save was triggered');
-});
-
-user.trigger('save');
+axios.get('http://localhost:3001/users/1');
